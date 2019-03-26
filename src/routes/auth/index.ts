c67579b5e7fea84f968a6ea04f63b6b7dd62de15
Alexey Sadovin <avsadovin@gmail.com>
2019-03-26 17:54:06 +0300

    Update auth routes

--- src/routes/auth/index.ts
+++ src/routes/auth/index.ts
@@ -1,5 +1,10 @@
 import * as express from 'express';
 
+import createController from './../../../core/rest/controller/createController';
+import RegisterController from './controllers/RegisterController';
+
 const router = express.Router({});
+
+router.post('/register', createController(RegisterController));
 
 export default router;
