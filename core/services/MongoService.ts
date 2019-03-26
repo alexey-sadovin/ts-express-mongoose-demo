354156adf0a640ac472e8ec35cd0498509e30f74
Alexey Sadovin <avsadovin@gmail.com>
2019-03-26 20:34:35 +0300

    Add MongoService

---
+++ core/services/MongoService.ts
@@ -0,0 +1,40 @@
+import * as mongoose from 'mongoose';
+import {Connection} from "mongoose";
+
+export default class MongoService {
+  private connection: Connection = mongoose.connection;
+
+  constructor(
+    private readonly dbHost: string,
+    private readonly dbPort: string,
+    private readonly dbName: string
+  ) {
+    this.dbHost = dbHost;
+    this.dbPort = dbPort;
+    this.dbName = dbName;
+  }
+
+  public async connect(): Promise<any> {
+    const connectionPromise = new Promise(resolve =>
+      this.connection.on('connected', () => {
+        console.log('Mongo connected');
+        resolve();
+      }));
+
+    this.connection.on('reconnected', () => {
+      console.log('Mongo reconnected');
+    });
+
+    mongoose
+      .connect(this.composeConnectionUrl(), {useNewUrlParser: true})
+      .catch((err: Error) => {
+        console.error('Mongo connection failed', err);
+      });
+
+    return connectionPromise;
+  }
+
+  private composeConnectionUrl(): string {
+    return `mongodb://${this.dbHost}:${this.dbPort}/${this.dbName}`;
+  }
+}
